6bd41ff10539e1b11c0ee3b7ae5879b128df92c4
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-04-26 15:07:38 +0200

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var nome = prompt("Ciao, qual è il tuo nome?");
+console.log(nome);
+var cognome = prompt("e il tuo cognome?");
+console.log(cognome);
+var colore = prompt("Qual è il tuo colore preferito?");
+console.log(colore);
+var numero = 21;
+
+
+document.getElementById("passwd").innerHTML = nome + cognome + colore + numero;
