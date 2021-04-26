acf842b191dd8c2406366dd51c35ef8683503419
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-04-26 17:00:07 +0200

    Aggiunta grafica

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var nome = prompt("Ciao, qual è il tuo nome?");
+var nome = prompt("Ciao! Qual è il tuo nome?");
 console.log(nome);
 var cognome = prompt("e il tuo cognome?");
 console.log(cognome);
@@ -6,5 +6,7 @@ var colore = prompt("Qual è il tuo colore preferito?");
 console.log(colore);
 var numero = 21;
 
+document.getElementById("name").innerHTML = nome;
+document.getElementById("surname").innerHTML = cognome;
 
 document.getElementById("passwd").innerHTML = nome + cognome + colore + numero;
